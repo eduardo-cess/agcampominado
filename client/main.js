db685827fdd46b1978903de40a2901779ede8bdd
Eduardo Santiago <eeduardosantiago@gmail.com>
2016-10-01 16:26:57 -0300

    algoritmo para um ensaio(meteor)

--- client/main.js
+++ client/main.js
@@ -4,36 +4,25 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 
-Template.main.onCreated(function mainOnCreated() {
-	iniciarPopulacao(2);
-	console.log(populacao);
-
-});
-
-Template.main.events({
-	// 'click .reiniciar'(event, instance) {
-    
- //  },
-});
-
-var populacao = [], populacaoFitness = [];
+var contGeracao = 0;
+var populacao = new Array(), populacaoFitness = [], normFitness = [], individuoMaisApto = [];
+var melhoresPorGeracao = [], pioresPorGeracao = [];
 var arraySomaFitness = [], somaFitness = 0; 
 var pais = [], filhos = [];
 var qtdCromossomos = 81, qtdBombas = 19;
 var taxaMutacao = 0.05, taxaCruzamento = 0.8;
 var melhorFitness = 0;
 var mineField = [
-  [1,  2, -1,  1, 1, -1, 2, -1, 1],
-  [2, -1,  4,  2, 1,  1, 3,  2, 2],
-  [3, -1,  4, -1, 2,  1, 2, -1, 1],
-  [3, -1,  4,  2, 3, -1, 2,  1, 1],
-  [2, -1,  3,  2,-1,  3, 3,  1, 1],
-  [1,  1,  2, -1, 3, -1, 2, -1, 1],
-  [1,  1,  2,  1, 3,  2, 4,  2, 2],
-  [1, -1,  1,  1, 2, -1, 3, -1, 2],
-  [1,  1,  1,  1,-1,  2, 3, -1, 2] // -1 == bomba
+  1,  2, -1,  1, 1, -1, 2, -1, 1,
+  2, -1,  4,  2, 1,  1, 3,  2, 2,
+  3, -1,  4, -1, 2,  1, 2, -1, 1,
+  3, -1,  4,  2, 3, -1, 2,  1, 1,
+  2, -1,  3,  2,-1,  3, 3,  1, 1,
+  1,  1,  2, -1, 3, -1, 2, -1, 1,
+  1,  1,  2,  1, 3,  2, 4,  2, 2,
+  1, -1,  1,  1, 2, -1, 3, -1, 2,
+  1,  1,  1,  1,-1,  2, 3, -1, 2 // -1 == bomba
 ];
-
 
 function iniciarPopulacao(qtdIndividuos){
 	for(i in _.range(qtdIndividuos)){
@@ -49,33 +38,35 @@ function iniciarPopulacao(qtdIndividuos){
 }
 
 function avaliar(){
+	populacaoFitness = [];
 	for(i in populacao){
 		var fitness = 0;
 		for(j in populacao[i]){
-			if(populacao[j]==1 && mineField[j]==-1)
+			if(populacao[i][j]==1 && mineField[j]==-1)
 				fitness++;
-			if(populacao[j]==0 && mineField[j]!=-1)
+			if(populacao[i][j]==0 && mineField[j]!=-1)
 				fitness++;
 		}
-		if(fitness > melhorFitness)
+		if(fitness > melhorFitness){
 			melhorFitness = fitness;
+			individuoMaisApto = populacao[i];
+		}
 		populacaoFitness.push(fitness);
 	}
 }
 
 function selecao(somaFitness){
-	var rand = m.random(0,somaFitness);
-
+	var rand = math.random(0,somaFitness);
 	for(i in arraySomaFitness){
 		if (arraySomaFitness[i] >= rand){
 			pais.push(populacao[i]);
-			rand = m.random(0,somaFitness);
+			rand = math.random(0,somaFitness);
 		}
 		if(pais.length == 2)
 			break;
 	}
 	if(pais.length < 2)
-		selecionar(somaFitness);
+		selecao(somaFitness);
 }
 
 function roleta(){
@@ -104,36 +95,92 @@ function cruzamento(){//cruxamento binário
 		pais = [];
 }
 
-function reproducao(){
+function reproduzir(){
 	filhos=[];
 	roleta();
-	selecionar(somaFitness);
-
+	selecao(somaFitness);
 	while(filhos.length < populacao.length){
 		if(Math.random() <= taxaCruzamento){
 			cruzamento();
-			selecionar(somaFitness);
+			selecao(somaFitness);
 		}
 	}
-
-	// if (tipo_ag == 2 || tipo_ag == 4 || tipo_ag == 6)
-	// 	this.filhos[0] = this.mais_apto;//elitismo
-
-	populacaoFitness = [];
+	filhos[0] = individuoMaisApto;//elitismo
+	//console.log(individuoMaisApto);
 	populacao = [];
 	populacao = filhos;
-
 	mutacao();
+	retirarExcessoBombasPopulacao();
 }
 
 function mutacao(){
 	for(i in populacao){
-		//if(i > 0){
-			for(j in populacao[i]){
-				if(Math.random() <= taxaMutacao)
-					populacao[i][j] = (populacao[i][j] == 0) ? 1 : 0;
-			}
-		//}
+		if(populacaoFitness[i] != qtdCromossomos){
+			if(i>0 && populacaoFitness[i] < 79){
+				for(j in populacao[i]){
+					if(Math.random() <= taxaMutacao)
+						populacao[i][j] = (populacao[i][j] == 0) ? 1 : 0;
+				}
+			}	
+		}else break;
 	}
 }
 
+function retirarExcessoBombasPopulacao(){
+	for(i in populacao){
+		if(populacaoFitness[i] != qtdCromossomos)
+			retiraBombasIndividuo(populacao[i]);
+		else break;
+	}
+}
+
+function retiraBombasIndividuo(individuo){
+	var bombas = countBombasIndividuo(individuo);
+	if(bombas > qtdBombas)
+		while(bombas > qtdBombas){
+			var random = math.randomInt(qtdCromossomos);
+			if(individuo[random] == 1){
+				if(mineField[random] != -1)
+					individuo[random] = 0;
+			}
+			bombas = countBombasIndividuo(individuo);
+		}			
+}
+
+function countBombasIndividuo(individuo){
+	count = 0;
+	for(i in individuo){
+		if(individuo[i] == 1)
+			count++;
+	}
+	return count;
+}
+
+
+Template.main.onCreated(function mainOnCreated() {
+});
+
+Template.main.events({
+	'click .iniciarAg'(event, instance) {
+		var start = new Date();
+		iniciarPopulacao(100);
+		avaliar();
+		for(geracao in _.range(200)){
+			contGeracao = geracao + 1;
+			reproduzir();
+			avaliar();
+			if(melhorFitness == 81)
+				var time = new Date() - start;
+			$('#statusAg').html("geração: "+geracao+" , melhor indivíduo: "+melhorFitness);
+		}
+		if(!time)
+			$('#statusAg').html("Não achou a solução");
+		else
+			$('#statusAg').html("Tempo até encontrar a solução: "+time/1000+" segundos");
+    for(i in populacao[0]){
+    	var id = '#'+i;
+    	if(populacao[0][i] != 1)
+    		$(id).trigger('click');
+    }
+  },
+});
